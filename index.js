30aa13b920d382bcd9110e68f1059e9b7a235564
danielescorsino <86973043+danielescorsino@users.noreply.github.com>
2021-07-13 14:38:13 +0200

    uploaded the validators for the expenses route

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const parser = require('body-parser')
 const logger = require('morgan')
+const { validateHeaderValue }= require('http')
+const { body, validationResult } = require('express-validator')
+//const  body-parser = require('body-parser')
 
 // database
 const expenses = []
@@ -10,17 +13,43 @@ const app = express()
   .use(parser.json())
   .use(express.static('web'))
 
-app.post('/expenses', (req, res) => {
-  const expense = req.body
-  expense.id = expenses.length
-  expenses.push(expense)
-  res.setHeader('Location', `/expenses/${expense.id}`) // fixme: this route is not yet implemented
-  res.status(201).json(expense)
+
+app.post('/expenses', 
+  body('username').isString(), 
+  body('description').isString(),
+  body('amount').isNumeric(),
+  body('participants').isString(),
+  body('date').isLength({min: 10 }).isDate(),
+  //body('image').isAlphanumeric(), 
+  (req, res) => {   
+  console.log(req.body)
+  const err = validationResult(req);  
+  if (!err.isEmpty()) {
+    console.log('The request is not valid', err)
+    return res.status(400).json(err);}
+  else { const expense = req.body
+    expense.id = expenses.length
+    expenses.push(expense)
+    res.setHeader('Location', `/expenses/${expense.id}`) // fixme: this route is not yet implemented
+    res.status(201).json(expense)
+  }
+  /*Expenses.create({
+    username: req.body.username,
+    description: req.body.description,
+    amount: req.body.amount,
+    participants: req.body.participants,
+    date: req.body.date,}).then(expenses => res.json(expenses));*/
 })
 
 app.get('/expenses', (req, res) => {
+  //const err = validateHeaderValue('Content-Type', 'application/json')
+  if (!err.isEmpty()){
+    console.log('The request is not valid', err)
+    return res.status(400).json(err);
+  }
+  else{
   res.setHeader('Content-Type', 'application/json')
-  res.json(expenses)
+  res.json(expenses)}
 })
 
 app.get('/*', (req, res) => {
