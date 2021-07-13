fbaf4f3c51c0f5f07deceacc0eeeae169ede6d7d
gitMelk <8224240+gitMelk@users.noreply.github.com>
2021-07-13 15:33:48 +0200

    data validation
    to do: 
    - check the image
    - show an alert

--- index.js
+++ index.js
@@ -12,20 +12,54 @@ const app = express()
 
 app.post('/expenses', (req, res) => {
   const expense = req.body
+
+  if(expense.username =="" ||
+      expense.description =="" ||
+      expense.amount == "NaN" ||
+      expense.participants =="")
+  { 
+    //window.alert("Empty field(s)!!")
+    res.status(400).end("Bad request")
+  }
+  
+  // to-do check the image and eventually put a default image
   expense.id = expenses.length
   expenses.push(expense)
   res.setHeader('Location', `/expenses/${expense.id}`) // fixme: this route is not yet implemented
   res.status(201).json(expense)
 })
 
+
 app.get('/expenses', (req, res) => {
   res.setHeader('Content-Type', 'application/json')
   res.json(expenses)
 })
 
+// ##############
+app.get('/expenses/:id', (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  res.status(200)
+  let tid = parseInt(req.params.id)
+  res.json(expenses.find( x => x.id === tid))
+})
+
+app.delete('/expenses/:id', (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  res.status(200)
+  let tid = parseInt(req.params.id)
+  let index = expenses.findIndex( x => x.id === tid)
+  expenses.splice(index, 1)
+  res.end()
+})
+
+
+// #####
+
+
 app.get('/*', (req, res) => {
   res.status(404).end('The requested route does not exist!')
 })
+
 
 module.exports = app.listen(3000, function () {
   console.log('* server started on http://localhost:3000')
